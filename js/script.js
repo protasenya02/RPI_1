ddaca76ac4aea02c70501278fc8220b645e7ae4d
Valentine Protasenya <protasenya02@icloud.com>
2021-04-15 20:40:35 +0300

    GET request modified

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+window.onload = () => {
+    createModal();
+};
+
+function createModal() {
+
+    const modalTemplate = getModalTemplate();
+    document.body.innerHTML += modalTemplate;
+
+    const requestURL = 'https://rpi-lab2.herokuapp.com/api/message';
+    loadRequest(requestURL);
+}
+
 function loadRequest(url) {
 
     let request = new XMLHttpRequest();
@@ -9,6 +22,7 @@ function loadRequest(url) {
     request.onload = function () {
         outputRequest(request.response);
     }
+
 }
 
 function outputRequest(request) {
@@ -17,11 +31,11 @@ function outputRequest(request) {
     let modalBody = document.querySelector(".modal_body");
     let bodyText = "";
 
-    Object.keys(obj).forEach(function(key) {
+    Object.keys(obj).forEach(function (key) {
         const messageTemplate = getMessageTemplate({
             name: obj[key].userName,
             message: obj[key].message,
-        });
+            });
         bodyText += messageTemplate;
     });
 
@@ -33,5 +47,22 @@ function getMessageTemplate(userData) {
        <p class="modal_text">
         <span class="user_name">${userData.name}</span>: ${userData.message}
        </p>
+  `;
+}
+
+function getModalTemplate() {
+    return `
+        <div id="openModal" class="modal">
+            <div class="modal_dialog">
+                <div class="modal_content">
+                    <div class="modal_header">
+                        <h3 class="modal_title">Results of GET request</h3>
+                        <a href="#close" title="Close" class="close">×</a>
+                    </div>
+                    <div class="modal_body">
+                    </div>
+                </div>
+            </div>
+       </div>
   `;
 }
